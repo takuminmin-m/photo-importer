549557d5ce7e882f837638c94f05cddf253baf49
takuminmin-m <takuminmin0718@gmail.com>
2023-01-03 23:10:35 +0900

    add detect target file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
+use std::collections::HashSet;
 use std::env;
 use std::fs;
+use std::path::PathBuf;
 
 use crate::camera_dir::CameraDir;
 mod camera_dir;
 
+
 fn main() {
+    let DEFAULT_TARGET_PATH: String = "~/Pictures".to_string();
+    let DEFAULT_ENABLED_EXT: Vec<String> = vec![
+        "jpg".to_string(),
+        "JPG".to_string(),
+        "jpeg".to_string(),
+        "JPEG".to_string(),
+        "png".to_string(),
+        "PNG".to_string(),
+        "mp4".to_string(),
+        "MP4".to_string(),
+    ];
+
+
     let args: Vec<String> = env::args().collect();
     assert!(
         args.len()-1==1 || args.len()-1==2,
@@ -15,16 +31,23 @@ fn main() {
     let target_path = if args.len()-1 == 2 {
         &args[2]
     } else {
-        "~/Pictures"
+        &DEFAULT_TARGET_PATH
     };
     let camera_path = &args[1];
-    let enabled_ext_str = vec!["jpg", "JPG", "jpeg", "png"];
-    let mut enabled_ext = Vec::<String>::new();
-    for e in enabled_ext_str.iter() {
-        enabled_ext.push(e.to_string());
-    }
+    let enabled_ext = &DEFAULT_ENABLED_EXT;
 
-    let camera_dir = CameraDir::new(camera_path, &enabled_ext);
+    let camera_dir = CameraDir::new(camera_path, enabled_ext);
+    let target_dir = CameraDir::new(target_path, enabled_ext);
 
     println!("{:?}", camera_dir);
+    println!("{:?}", target_dir);
+
+    let target_dir_photos: HashSet<PathBuf> = target_dir.photo_filenames.iter().cloned().collect();
+    let mut target_photos = Vec::<&PathBuf>::new();
+    for f in camera_dir.photo_filenames.iter() {
+        if !target_dir_photos.contains(f) {
+            target_photos.push(f);
+        }
+    }
+
 }
